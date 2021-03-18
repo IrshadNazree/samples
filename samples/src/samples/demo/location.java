17cf5adba5502d240ac689e49d982b947db2e6d8
Irshad Nazree <inazree370@gmail.com>
2021-03-18 13:42:53 +0800

    Commit

---
+++ samples/src/samples/demo/location.java
@@ -0,0 +1,13 @@
+package samples.demo;
+
+public class location {
+	 private int number;
+
+	 public int getNumber() {
+		 return this.number;
+	 }
+
+	 public void setNumber(int num) {
+		 this.number = num;
+	 }
+}
