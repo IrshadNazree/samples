ff9cfea4a4b176f9e8dda907fc9f3279c197342a
Irshad Nazree <inazree370@gmail.com>
2021-03-18 14:18:13 +0800

    Atr addition

--- samples/src/samples/demo/location.java
+++ samples/src/samples/demo/location.java
@@ -2,6 +2,8 @@ package samples.demo;
 
 public class location {
 	 private int postcode;
+	 private String address;
+	 private String state;
 
 	 public int getPost() {
 		 return this.postcode;
@@ -9,5 +11,21 @@ public class location {
 
 	 public void setPost(int post) {
 		 this.postcode = post;
+	 }
+	 
+	 public String getAdd() {
+		 return this.address;
+	 }
+
+	 public void setAdd(String add) {
+		 this.address = add;
+	 }
+	 
+	 public String getState() {
+		 return this.state;
+	 }
+
+	 public void setState(String state) {
+		 this.state = state;
 	 }
 }
